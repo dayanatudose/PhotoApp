773d19923f888e28ddbd582d9b138d39d9e5d5eb
dayanatudose <daiana.tudose@student.upt.ro>
2021-05-17 00:13:53 +0300

    commit3

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/users').then(response => {
+      setUsers(response.data);
+    })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {users.map((user: any) => (
+            <li key={user.id}>{user.username}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
