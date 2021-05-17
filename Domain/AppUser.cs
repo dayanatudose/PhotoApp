a0b0e3efd7bd3a26113023d22d125f77b25169a3
dayanatudose <daiana.tudose@student.upt.ro>
2021-05-17 22:42:06 +0300

    m

---
+++ Domain/AppUser.cs
@@ -0,0 +1,12 @@
+namespace Domain
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email {get; set;}
+        public string Telephone {get; set;}
+        public string Description {get; set;}
+        
+    }
+}
